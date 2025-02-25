bfde82a7598544bb7e0e8b7d7deae57c898783a4
shantanu saraf <shantanusaraf4@gmail.com>
2025-02-25 14:28:26 +0530

    update to groq model for code ai tool

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,35 +1,28 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
-
 import { checkSubscription } from "@/lib/subscription";
 import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+import Groq from "groq-sdk";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-const openai = new OpenAIApi(configuration);
-
-const instructionMessage: ChatCompletionRequestMessage = {
+const instructionMessage = {
   role: "system",
   content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
 };
 
-export async function POST(
-  req: Request
-) {
+export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { messages  } = body;
+    const { messages } = body;
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    if (!configuration.apiKey) {
-      return new NextResponse("OpenAI API Key not configured.", { status: 500 });
+    if (!process.env.GROQ_API_KEY) {
+      return new NextResponse("Groq API Key not configured.", { status: 500 });
     }
 
     if (!messages) {
@@ -43,18 +36,18 @@ export async function POST(
       return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
     }
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [instructionMessage, ...messages]
+    const response = await groq.chat.completions.create({
+      messages: [instructionMessage, ...messages],
+      model: "llama-3.3-70b-versatile",
     });
 
     if (!isPro) {
       await incrementApiLimit();
     }
 
-    return NextResponse.json(response.data.choices[0].message);
+    return NextResponse.json(response.choices[0].message);
   } catch (error) {
-    console.log('[CODE_ERROR]', error);
+    console.log("[CODE_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
-};
+}
